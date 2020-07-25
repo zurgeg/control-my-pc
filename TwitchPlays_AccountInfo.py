82a86e1a998f08298b22ff3d3318c2240f697a66
winnerspiros <1675249+winnerspiros@users.noreply.github.com>
2020-07-25 14:19:00 +0300

    Update TwitchPlays_AccountInfo.py
    Cleaned comments for no reason

--- TwitchPlays_AccountInfo.py
+++ TwitchPlays_AccountInfo.py
@@ -1,9 +1,3 @@
-# DougDoug Note:
-# This file contains your Twitch account info for logging into the chat
 # Your oauth-token can be generated at http://twitchapps.com/tmi/
-
-# WARNING: DO NOT PUBLICLY SHARE THIS INFORMATION. THIS INFO IS USED TO ACCESS YOUR TWITCH ACCOUNT.
-
-# TODO: put your Twitch username and OAuth token here
-TWITCH_USERNAME = 'type name here'
-TWITCH_OAUTH_TOKEN = 'type oauth code here'
+TWITCH_USERNAME = 'Bot&StreamName'
+TWITCH_OAUTH_TOKEN = 'oauth:'
