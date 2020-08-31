569eb00e56cfffc41cc6ad98f073351f1cad1e27
maxbrd <me@fadedmax.net>
2020-08-31 19:26:20 -0400

    Update TwitchPlays_AccountInfo.py
    added array.

--- TwitchPlays_AccountInfo.py
+++ TwitchPlays_AccountInfo.py
@@ -1,5 +1,8 @@
 # Your oauth-token can be generated at http://twitchapps.com/tmi/
 TWITCH_USERNAME = 'Bot&StreamName'
 TWITCH_OAUTH_TOKEN = 'oauth:'
+# user configuration
+# -------------
+DEVS = ['maxlovetoby', 'controlmypc', 'forevertoo']
 # log all chat to console?
 LOG_ALL = 'true'
