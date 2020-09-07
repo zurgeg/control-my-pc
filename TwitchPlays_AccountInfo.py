a85586031082c9126f553c27c2e00aa7efe4f001
Max Bradley <maxclarkbradley@icloud.com>
2020-09-07 15:52:50 +0000

    added proper config

--- TwitchPlays_AccountInfo.py
+++ TwitchPlays_AccountInfo.py
@@ -3,12 +3,16 @@ TWITCH_USERNAME = 'Bot&StreamName'
 TWITCH_OAUTH_TOKEN = 'oauth:'
 # user configuration
 # -------------
-DEVS = ['maxlovetoby', 'controlmypc', 'forevertoo']
-# log all chat to console?
+DEVS = ['maxlovetoby', 'ne3k', 'controlmypc', 'joel_mcb', 'vsolars228']
+MODS = ['F1nn17_', 'ne3k', 'f1nn17_', 'TheMaddestPancake', 'Vsolars228', 'pocketzhungry', 'Wbubbler1',]
+# configuration
+# -------------
+# Log all chat messages in console?
 LOG_ALL = 'true'
 # Send start message to discord?
-START_MSG = 'true'
+START_MSG = 'false'
 # Send exceptions to discord?
-EXC_MSG = 'true'
+EXC_MSG = 'false'
 # Log all chat to a .log?
 LOG_PPR = 'true'
+# 
