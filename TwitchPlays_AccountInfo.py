7d93ad41796268efac03691beff6bdfc78f9b8af
fadedmax <maxclarkbradley@icloud.com>
2020-09-10 16:00:06 +0000

    fixed api and formatting

--- TwitchPlays_AccountInfo.py
+++ TwitchPlays_AccountInfo.py
@@ -1,21 +1,20 @@
 # Your oauth-token can be generated at http://twitchapps.com/tmi/
 TWITCH_USERNAME = 'Bot&StreamName'
 TWITCH_OAUTH_TOKEN = 'oauth:'
-# user configuration
-# -------------
-# to setup users, you will need a api website.
-#mod list: streamelements, forevertoo, pverte, wbubbler1, supersonicandmario, themaddestpancake, just_jotaro_kujo, ttv_pedro270707, smbfan_smm, densetsu___, hetytkanaal, stookygreenbonnie1, maxlovetoby, f1nn17_, fulquit, vsolars228, controlmybot, nightbot
 # configuration
 # -------------
+DEV_API = "https://api.cmpc.live/devs.txt"
+MOD_API = "https://api.cmpc.live/mods.txt"
 # Log all chat messages in console?
 LOG_ALL = 'true'
 # Send start message to discord?
-START_MSG = 'true'
+START_MSG = 'false'
 # Send exceptions to discord?
-EXC_MSG = 'true'
+EXC_MSG = 'false'
 # Log all chat to a .log?
 LOG_PPR = 'true'
-# webhook config
+# webhook configuration
+# -------------
 chatalerts = "https://discordapp.com/api/webhooks/741306005589327952/RMDc8zdyYG1BNuLjL2EDUIYVNwCJoJgwld8G8czXEwnp9kv_oGLMmv77RG5AoubrgfW8"
 chatrelay = "https://discordapp.com/api/webhooks/741316193369194506/slrRQYiTHPP6uNPeIflzIBuw6SQ5cZnsd_E6YHvl6BowBp-BPEVRl_cj0pN3TpYcxPcl"
 modtalk = "https://discordapp.com/api/webhooks/741311244308578392/7azdTbjAljUT0PTjBkTJzddh4i4vTj1H-_A1zJJVMZEYOIGWsP_1e60vTIJK-PQTOhEk"
