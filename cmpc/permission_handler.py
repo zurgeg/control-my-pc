5af8153154b66946752fce4f3e9a6e9172fcad0d
Callum Bartlett <callum@voxelfox.co.uk>
2020-10-25 04:09:24 +0000

    Fix permission handler attributes

--- cmpc/permission_handler.py
+++ cmpc/permission_handler.py
@@ -4,6 +4,8 @@ class Permissions(object):
     DEV_PERMISSION = 0b010
     SCRIPT_PERMISSION = 0b100
 
+    ALL_PERMISSIONS = 0b111111111111  # This doesn't need to be accurate it just needs to be bigger than everything else
+
     def __init__(self, **kwargs):
         self.value = 0
         for k, v in kwargs.items():
@@ -11,34 +13,34 @@ class Permissions(object):
 
     @property
     def moderator(self):
-        return self._moderator
+        return bool(self.value & self.MOD_PERMISSION)
 
     @moderator.setter
     def moderator(self, value):
         if value:
-            self.value = self.value | self.THING_PERMISSION
+            self.value = self.value | self.MOD_PERMISSION
         else:
-            self.value = self.value & (0b111111111111 ^ self.THING_PERMISSION)
+            self.value = self.value & (self.ALL_PERMISSIONS ^ self.MOD_PERMISSION)
 
     @property
     def developer(self):
-        return self._developer
+        return bool(self.value & self.DEV_PERMISSION)
 
     @developer.setter
     def developer(self, value):
         if value:
-            self.value = self.value | self.THING_PERMISSION
+            self.value = self.value | self.DEV_PERMISSION
         else:
-            self.value = self.value & (0b111111111111 ^ self.THING_PERMISSION)
+            self.value = self.value & (self.ALL_PERMISSIONS ^ self.DEV_PERMISSION)
 
     @property
     def script(self):
-        return self._script
+        return bool(self.value & self.SCRIPT_PERMISSION)
 
     @script.setter
     def script(self, value):
         if value:
-            self.value = self.value | self.THING_PERMISSION
+            self.value = self.value | self.SCRIPT_PERMISSION
         else:
-            self.value = self.value & (0b111111111111 ^ self.THING_PERMISSION)
+            self.value = self.value & (self.ALL_PERMISSIONS ^ self.SCRIPT_PERMISSION)
 
