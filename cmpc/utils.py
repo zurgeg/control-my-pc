6cf79faeaefdfcc50d201f9ffeea78de46cc95cd
fadedmax <maxclarkbradley@icloud.com>
2020-10-26 21:38:19 +0000

    add some docs to utils

--- cmpc/utils.py
+++ cmpc/utils.py
@@ -6,7 +6,7 @@ import requests
 import pyautogui
 import psutil
 
-
+# Check if we are on a mac or not
 if sys.platform == "darwin":
     pass
 else:
@@ -45,6 +45,7 @@ def get_size(value, suffix="B"):
 
 
 def direct_or_auto():
+    """Returns if we should use pydirectinput or pyautogui"""
     platform = get_platform()
     if platform == "darwin":
         return 'auto'
@@ -52,17 +53,20 @@ def direct_or_auto():
         return 'direct'
 
 
-def send_webhook(url, content):
+def send_webhook(url:str, content:str):
+    """Sends a webhook to discord, takes (url, message)"""
     data = {"content": content}
     requests.post(url, data=data)
 
 
 def send_error(url, error, msg, usr, channel):
+    """Sends a error to discord"""
     datatest = {"embeds": [{"title": "Script - Exception Occured","description": f"***Last Sent Message -*** {msg}\n\n***Exception Info -*** {error}\n\n[***Stream Link***](https://twitch.tv/{channel})","color": 1107600,"footer": {"text": 'User: ' + usr + ' - Channel: ' + channel,"icon_url": "https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg"}}]}
     requests.post(url, data=json.dumps(datatest), headers={"Content-Type": "application/json"})
 
 
 def move(*args):
+    """Moves the mouse with cross-platform support"""
     dor = direct_or_auto()
     if dor == "auto":
         pyautogui.move(*args)
@@ -71,10 +75,12 @@ def move(*args):
 
 
 def press(key):
+    """Presses a key (more functonality coming soon)"""
     pyautogui.press(key)
 
 
 def send_data(url, context):
+    """Dumps machine data, config, and api"""
     machine_stats = "\n\n".join([
         f"CPU Frequency: {round(int(psutil.cpu_freq().current) / 1000, 2)} GHz",
         f"Total Usage: {psutil.cpu_percent()}%",
