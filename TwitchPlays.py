18e6941c77018055d71d84727d4e117e51d147b1
JMcB#7918 <joel.mcbride1@live.com>
2020-11-06 20:19:02 +0000

    Add more info to Script - Online message

--- TwitchPlays.py
+++ TwitchPlays.py
@@ -67,7 +67,11 @@ else:
 
 # Send starting up message with webhook if in config.
 if config['options']['START_MSG']:
-    cmpc.send_webhook(config['discord']['systemlog'], 'Script Online')
+    cmpc.send_webhook(config['discord']['systemlog'],
+                      'Script - **Online**\n\n'
+                      f'[***Stream Link***](https://twitch.tv/{TWITCH_USERNAME})\n\n'
+                      f"**Environment -** {config['options']['DEPLOY']}",
+                      )
 
 
 def load_user_permissions(dev_list, mod_list):
