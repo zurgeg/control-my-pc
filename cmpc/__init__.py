0d45208458db44612c8abdad3c7833cc2d647c6d
JMcB#7918 <joel.mcbride1@live.com>
2020-11-11 21:14:48 +0000

    Fix cmpc init

--- cmpc/__init__.py
+++ cmpc/__init__.py
@@ -3,4 +3,4 @@ from cmpc.keyboard_keycodes import KeyboardKeycodes
 from cmpc.command_processor import CommandProcessor
 from cmpc.twitch_message import TwitchMessage
 from cmpc.permission_handler import Permissions
-from cmpc.twitch_connection import import TwitchConnection
+from cmpc.twitch_connection import TwitchConnection
