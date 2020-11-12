bf5e862078ef384d05964243d7166c030bb06b23
JMcB#7918 <joel.mcbride1@live.com>
2020-11-12 18:40:09 +0000

    TwitchConnection with twitchio

--- cmpc/twitch_connection.py
+++ cmpc/twitch_connection.py
@@ -1,6 +1,16 @@
-from python_twitch_irc import TwitchIrc
+from twitchio.ext.commands.bot import Bot
 
 
-class TwitchConnection(TwitchIrc):
-    def on_connect(self):
-        self.join(f'#{self._username}')
+class TwitchConnection(Bot):
+    def __init__(self, user, oauth, client_id=None, prefix=None):
+        if client_id is None:
+            client_id = 'CMPCscript (+https://cmpc.live/)'
+        if prefix is None:
+            # TODO: Change?
+            prefix = 'asdf'
+
+        super().__init__(irc_token=oauth, client_id=client_id, nick=user, prefix=prefix, initial_channels=[user])
+
+    # I don't know why this method is classed as necessary to implement but here it is.
+    async def event_pubsub(self, data):
+        pass
