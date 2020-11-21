edcfec78268e2bdefb45d841d227cd3dd16baa60
JMcB#7918 <joel.mcbride1@live.com>
2020-11-21 19:07:47 +0000

    @ devs in the error webhook message

--- cmpc/utils.py
+++ cmpc/utils.py
@@ -113,6 +113,7 @@ def send_error(url, error, t_msg, channel, environment, branch, branch_assumed):
         embed_description = embed_description + f'\n\n**Branch -** {branch}'
 
     data = {
+        'content': '<@&779783726196064323>',
         'embeds': [
             {
                 'title': 'Script - Exception Occurred',
