e9a522568e3570b5d806bd9f2dab7cc0bad548fb
JMcB#7918 <joel.mcbride1@live.com>
2020-12-04 08:03:27 +0000

    fstring instead of string addition for neatness

--- cmpc/command_processor.py
+++ cmpc/command_processor.py
@@ -389,7 +389,7 @@ class CommandProcessor:
                         }
                     ],
                     'username': message.username,
-                    'content': self.config['discord']['modalertping'] + ' https://twitch.tv/controlmypc',
+                    'content': f"{self.config['discord']['modalertping']} https://twitch.tv/controlmypc",
                 }
                 log.info('[MODALERT] Sending request...')
                 # TODO: Move this requests over to cmpc package, so that way we can check if there is even a webhook
