6b360fa58af025beab583427af8acc1916ea5d0b
JMcB <joel.mcbride1@live.com>
2021-03-12 15:13:40 +0000

    Create stub ws log method

--- cmpc/command_logging.py
+++ cmpc/command_logging.py
@@ -6,7 +6,7 @@ import cmpc.twitch_message
 
 
 class CommandLogging:
-    def __init__(self, bot, obs_file_name, obs_log_sleep_duration=None, obs_none_log_msg=None):
+    def __init__(self, bot, obs_file_name, obs_log_sleep_duration=None, obs_none_log_msg=None, use_websockets=False):
         self.bot = bot
         self.obs_file_name = obs_file_name
         if obs_log_sleep_duration is None:
@@ -16,6 +16,11 @@ class CommandLogging:
             obs_none_log_msg = 'nothing'
         self.obs_none_log_msg = obs_none_log_msg
 
+        if use_websockets:
+            self.obs_log_handler = self._obs_log_ws
+        else:
+            self.obs_log_handler = self._obs_log_executing_txt
+
     def log_to_discord(self, message: cmpc.twitch_message.TwitchMessage):
         requests.post(self.bot.config['discord']['chatrelay'],
                       json=message.get_log_webhook_payload(),
@@ -24,6 +29,11 @@ class CommandLogging:
     def _obs_log_executing_txt(self, obs_log_text):
         with open(self.obs_file_name, 'w', encoding='utf-8') as obs_file_handle:
             obs_file_handle.write(obs_log_text)
+
+    def _obs_log_ws(self, obs_log_text):
+        log.warning('Websocket logging not yet implemented')
+        self._obs_log_executing_txt(obs_log_text)
+        self.obs_log_handler = self._obs_log_executing_txt
 
     def log_to_obs(
             self, message: cmpc.twitch_message.TwitchMessage, none_log_msg=None, sleep_duration=None, none_sleep=False
@@ -35,11 +45,11 @@ class CommandLogging:
             sleep_duration = self.obs_log_sleep_duration
 
         if message is None:
-            self._obs_log_executing_txt(none_log_msg)
+            self.obs_log_handler(none_log_msg)
             if none_sleep:
                 time.sleep(sleep_duration)
         else:
-            self._obs_log_executing_txt(message.get_log_string())
+            self.obs_log_handler(message.get_log_string())
 
             time.sleep(sleep_duration)
             log.info(message.get_log_string())
