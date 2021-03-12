c15932e140708e8e7e78e253f4dccda59d91e76f
JMcB <joel.mcbride1@live.com>
2021-03-12 15:10:00 +0000

    Make obs logging with executing.txt its own private method

--- cmpc/command_logging.py
+++ cmpc/command_logging.py
@@ -21,6 +21,10 @@ class CommandLogging:
                       json=message.get_log_webhook_payload(),
                       headers={'User-Agent': self.bot.config['api']['useragent']})
 
+    def _obs_log_executing_txt(self, obs_log_text):
+        with open(self.obs_file_name, 'w', encoding='utf-8') as obs_file_handle:
+            obs_file_handle.write(obs_log_text)
+
     def log_to_obs(
             self, message: cmpc.twitch_message.TwitchMessage, none_log_msg=None, sleep_duration=None, none_sleep=False
     ):
@@ -31,13 +35,11 @@ class CommandLogging:
             sleep_duration = self.obs_log_sleep_duration
 
         if message is None:
-            with open(self.obs_file_name, 'w', encoding='utf-8') as obs_file_handle:
-                obs_file_handle.write(none_log_msg)
+            self._obs_log_executing_txt(none_log_msg)
             if none_sleep:
                 time.sleep(sleep_duration)
         else:
-            with open(self.obs_file_name, 'w', encoding='utf-8') as obs_file_handle:
-                obs_file_handle.write(message.get_log_string())
+            self._obs_log_executing_txt(message.get_log_string())
 
             time.sleep(sleep_duration)
             log.info(message.get_log_string())
