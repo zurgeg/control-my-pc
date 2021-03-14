a47d74a924944e198dafcf225d70871f3b74a2a6
JMcB <joel.mcbride1@live.com>
2021-03-14 16:26:57 +0000

    Include the broadcaster when checking for moderators

--- cmpc/mod_rota.py
+++ cmpc/mod_rota.py
@@ -108,7 +108,7 @@ class ModRota:
             log.error('Unable to get chatters list for channel in mod presence check.')
             return False
 
-        for user in chatters.moderators:
+        for user in chatters.moderators+chatters.broadcaster:
             user_permissions = self.bot.user_permissions_handler.get(user, cmpc.Permissions())
             if user_permissions.moderator or user_permissions.developer:
                 return True
