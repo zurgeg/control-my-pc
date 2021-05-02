df5ce994077004f7433752603d8aabecb87049ab
JMcB <joel.mcbride1@live.com>
2021-05-02 19:59:37 +0100

    Left the modtools ban exemption for mods commented out, oops
    Fixed. Thankfully it didn't affect anything because we're only currently using account age checking, not manual banning.

--- TwitchPlays.py
+++ TwitchPlays.py
@@ -217,8 +217,7 @@ class TwitchPlays(twitchio.ext.commands.bot.Bot):
             if self.modtools_on:
                 # Check if the user is allowed to run commands
                 # Don't bother checking for moderators or developers
-                # if not (user_permissions.moderator or user_permissions.developer):
-                if True:
+                if not (user_permissions.moderator or user_permissions.developer):
                     if not await self.modtools.check_user_allowed(message.author.id):
                         await self.modtools.notify_ignored_user(message)
                         log.info(f'Ignored message from {twitch_message.username} due to account age or deny list.')
